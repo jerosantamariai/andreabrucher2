6070503b73dc7b7c64264814255553ad17612f64
jerosantamariai <jerosantamariai@gmail.com>
2021-03-11 18:10:49 -0300

    ew dvance including photo section and modify rrss

---
+++ front/src/components/fotosterreno.jsx
@@ -0,0 +1,10 @@
+import Arrayan1 from '../img/enterreno/arrayan1.jpg';
+import Arrayan2 from '../img/enterreno/arrayan2.jpg';
+import Arrayan3 from '../img/enterreno/arrayan3.jpg';
+import Arrayan4 from '../img/enterreno/arrayan4.jpg';
+import Arrayan5 from '../img/enterreno/arrayan5.jpg';
+import Arrayan6 from '../img/enterreno/arrayan6.jpg';
+
+export default [
+    Arrayan1, Arrayan2, Arrayan3, Arrayan4, Arrayan5, Arrayan6,
+]
