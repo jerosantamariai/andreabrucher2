6131c3dc0ee90cad07038ed33680208db2652636
jerosantamariai <jerosantamariai@gmail.com>
2021-03-12 15:44:33 -0300

    adding a lot of things

--- front/src/views/landing/conoceme.jsx
+++ front/src/views/landing/conoceme.jsx
@@ -3,23 +3,32 @@ import React from 'react';
 const Conoceme = prop => {
     return (
         <div className="conoceme no-gutters" id="conoceme">
-            <div className="row no-gutters">
+            <div className="row no-gutters my-5 ml-5">
                 <div className="col-12">
                     <h1>CONÓCEME</h1>
-                    <p>Me considero una colaboradora importante de Cristobal Lira, que cunta con un proyecto concreto y
+                </div>
+            </div>
+            <div className="conocemecontainer d-flex">
+                <div className="row no-gutters">
+                    <div className="col-7">
+                        <p>Me considero una colaboradora importante de Cristobal Lira, que cunta con un proyecto concreto y
 real para el desarrollo de la comuna.</p>
-                    <p>Soy Andrea Brucher, tengo 46 años, ingeniera Comercial con mencion en administracion de empresas,
-                    casada hace 22 años y soy mamá de 3 niñitas. Soy republicana y hoy candidata a concejal por la
+                        <p>Soy Andrea Brucher, tengo 46 años, ingeniera Comercial con mencion en administracion de empresas,
+                        casada hace 22 años y soy mamá de 3 niñitas. Soy republicana y hoy candidata a concejal por la
 comuna de Lo Barnechea. Patriota y para mi Chile y su gente esta primero.</p>
-                    <p>Me interesa que me conozcan como una mujer de derecha, verdadera, valiente, resolutiva, sicenra
+                        <p>Me interesa que me conozcan como una mujer de derecha, verdadera, valiente, resolutiva, sicenra
 y espontanea.</p>
-                    <p>Vivo hace 10 años en la comuna. Me motiva trabajar en base a la unidad y la trasparecia,
-                    por las necesidades y mejoras para hacer de Lo Barnechea una comuna de excelencia para que todas
-                    las personas tengan oportunidad de estudiar y emprender, donde se respete el estado de derecho y
+                        <p>Vivo hace 10 años en la comuna. Me motiva trabajar en base a la unidad y la trasparecia,
+                        por las necesidades y mejoras para hacer de Lo Barnechea una comuna de excelencia para que todas
+                        las personas tengan oportunidad de estudiar y emprender, donde se respete el estado de derecho y
 la seguridad sea el eje principal para todos los ciudadanos.</p>
-                    <p>Estoy abierta al dialogo y me pueden contactar a través de mis redes, donde pondré los principios
-                    que me identifican y las propuestas que les traigo para lograr ser tu candidata a concejal en
+                        <p>Estoy abierta al dialogo y me pueden contactar a través de mis redes, donde pondré los principios
+                        que me identifican y las propuestas que les traigo para lograr ser tu candidata a concejal en
 estas elecciones del 11 de abril de 2021.</p>
+                    </div>
+                    <div className="col-5">
+                        <img src="https://picsum.photos/200" alt="foto" />
+                    </div>
                 </div>
             </div>
         </div>
--- front/src/views/landing/prensa.jsx
+++ front/src/views/landing/prensa.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import Gente from '../../img/breaks/gente.png'
 
 const Prensa = props => {
-    return(
-        <div className = "prensa" id = "prensa" >
-            <h1>PRENSA</h1>
-            
+    return (
+        <div className="prensa" id="prensa" >
+            <div className="row no-gutters">
+                <div className="col-12">
+                    <h1>PRENSA</h1>
+                </div>
+            </div>
+            <div className="row no-gutters">
+                <div className="col-12">
+                    <h1>INFO</h1>
+                </div>
+            </div>
+            <div className="row no-gutters">
+                <div className="col-12">
+                    <img src={Gente} alt="Gente" className="break" />
+                </div>
+            </div>
         </div >
     );
 }
